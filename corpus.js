f3ef2419916904848ce326451396c75b3be8aac5
matrixbirds <yooobuntu@gmail.com>
2019-01-28 00:20:59 +0800

    chore. scratch

---
+++ corpus.js
@@ -0,0 +1,13 @@
+// コーパス
+
+
+// REFERENCES: probably not mean in this scenarios
+// https://en.wikipedia.org/wiki/Sanskrit
+// https://zh.wikipedia.org/wiki/%E6%82%89%E6%9B%87%E5%AD%B8
+
+const alphbets = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+
+function alphabet_ja () {
+    const alphabet_obj = {};
+    return alphabet_obj;
+}
